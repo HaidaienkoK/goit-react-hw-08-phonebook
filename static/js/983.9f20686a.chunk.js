b6bfbd9790ce01cd77783250948137cb9e9d0edf
HaidaienkoK <112855935+HaidaienkoK@users.noreply.github.com>
2023-05-26 16:19:43 +0000

    Deploying to gh-pages from @ HaidaienkoK/goit-react-hw-08-phonebook@17bd885c3c63e0a479c60f49be51ad20561f006f 🚀

---
+++ static/js/983.9f20686a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(184),c={container:{display:"flex",flexDirection:"column",alignItems:"center"}};function o(){return(0,r.jsx)("div",{style:c.container,children:(0,r.jsx)("h1",{children:"Contact book"})})}}}]);
+//# sourceMappingURL=983.9f20686a.chunk.js.map
